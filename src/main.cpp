7b471394dffa4ac1e8a6845237d0f91df6a7a5f7
Dmitry Sokolov <assador@gmail.com>
2026-08-23 01:33:52 +0300

    ci: fix GitHub Actions workflow for AppImage.

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,6 @@ static Config config;
 static AppState state(config);
 
 int main(int argc, char** argv) {
+	setenv("GDK_BACKEND", "wayland", 1);
 	return run_application(argc, argv, state, config);
 }
